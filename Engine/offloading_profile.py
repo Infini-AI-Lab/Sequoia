0a8d0006c07ba484c2d05ec3c6e77d738c7fe173
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-05-19 18:14:40 -0400

    Update offloading_profile.py

--- Engine/offloading_profile.py
+++ Engine/offloading_profile.py
@@ -3,7 +3,7 @@ import argparse
 import time
 import torch
 parser = argparse.ArgumentParser()
-parser.add_argument('--model', type=str, default="meta-llama/Llama-2-70b-hf",help='model')
+parser.add_argument('--model', type=str, default="meta-llama/Meta-Llama-3-70B-Instruct",help='model')
 parser.add_argument('--T', type=int, default=100, help='repeat times')
 parser.add_argument('--B', type=int, default=1, help='batch size')
 parser.add_argument('--P', type=int, default=128, help='prefix length')
