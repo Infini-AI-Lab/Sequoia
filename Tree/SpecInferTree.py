2bee26a70bb3e4a252006e9b7fa5d2202b2a80de
dreaming-panda <64370186+dreaming-panda@users.noreply.github.com>
2024-05-22 03:35:06 -0400

    Update SpecInferTree.py

--- Tree/SpecInferTree.py
+++ Tree/SpecInferTree.py
@@ -158,7 +158,7 @@ class SpecInferTree(Tree):
             if p[token] >= r * q[token]:
                 return (pos + (self.ground_truth_len - 1), None)
             else:
-                p = self.residual_graph(p, q)
+                p = get_residual(p, q)
         return (-1, p)
 
 
